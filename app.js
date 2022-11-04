f7f288c04291d83327c01a20be294e7e9a5b1173
Harsha-kalava <harshcode16@gmail.com>
2022-11-04 23:14:58 +0530

    First express task

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.use((req,res,next)=>{
+    console.log('In the middleware')
+    next()
+})
+
+app.use((req,res,next)=>{
+    console.log('In the other middleware')
+    res.send('<h1>Hello server</h1>')   
+})
+
+app.listen(3000)
